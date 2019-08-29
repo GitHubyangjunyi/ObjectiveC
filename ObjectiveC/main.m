ed22b0cea72a4ac81638c0b4b44a9b8d95cb3e75
杨俊艺 <1209032479@qq.com>
2019-08-30 07:49:24 +0800

    Initial Commit

---
+++ ObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjectiveC
+//
+//  Created by 杨俊艺 on 2019/8/30.
+//  Copyright © 2019 杨俊艺. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
